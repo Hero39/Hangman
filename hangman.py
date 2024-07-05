aea3de5c2c000d6e70169676180b3492f78b98cc
Hero Brouwer <hero.brouwer.student@uva.nl>
2024-07-05 18:09:59 +0200

    update hangman

--- hangman.py
+++ hangman.py
@@ -7,128 +7,150 @@
 import random
 
 # List of all possible drawings for Hangman
-hangman_drawings = [
-'''
-|
-|
-|
-|
-|
-|
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|
-|
-|
-|
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|       O
-|
-|
-|
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|       O
-|       |
-|       |
-|
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|       O
-|       |\\
-|       |
-|
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|       O
-|      /|\\
-|       |
-|
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|       O
-|      /|\\
-|       |
-|        \\
-|
-|
-============
-''',
-'''
-=========
-|       |
-|       |
-|       O
-|      /|\\
-|       |
-|      / \\
-|
-|
-============
-'''
-]
+def show_hangman(tries):
+    hangman_drawings = [
+    '''
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |
+        |
+        |
+        |
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |       O
+        |
+        |
+        |
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |       O
+        |       |
+        |       |
+        |
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |       O
+        |       |\\
+        |       |
+        |
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |       O
+        |      /|\\
+        |       |
+        |
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |       O
+        |      /|\\
+        |       |
+        |        \\
+        |
+        |
+        ============
+    ''',
+    '''
+        =========
+        |       |
+        |       |
+        |       O
+        |      /|\\
+        |       |
+        |      / \\
+        |
+        |
+        ============
+    '''
+    ]
+    return hangman_drawings[tries]
+
 words = ['science', 'deadlock', 'starvation', 'kickboxing']
 
 # Chose a random word. For future: use a dictionary.
 def choose_word():
-    return random.choice(words)
+    word = random.choice(words)
+    return word.upper()
 
-# Splits the word
-def split_word(word):
-    return [x for x in word]
+def game(word):
+    word_completion = "_" * len(word)
+    guessed_word = False
+    guessed_letters = []
+    guessed_words = []
+    tries_left = 0
 
-word = split_word(choose_word())
-print(word)
+    print("Welcome to Hangman!")
+    print("You can guess 7 letters wrong before you lose.")
+    print("Good Luck!")
+    print(show_hangman(tries_left))
+    print(word_completion)
 
-# Need an extra variable to remember the word!!
+    while not guessed_word and tries_left < 7:
+        guess = input("Guess the letter or word: ").upper()
 
+        if len(guess) > 1 and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word!")
 
-# Replaces the letters with '_'
-def replace_letters(word):
-    for x in range(len(word)):
-        word[x] = '_'
-    return word
+                guessed_words.append(guess)
 
-print(replace_letters(word))
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed this letter!")
+            elif word.count(guess) == 0:
+                tries_left += 1
 
-print(' '.join(word))
+        else:
+            print("Input contains symbols that are not in the alphabet!\n")
+            print("")
 
-for draw in hangman_drawings:
-    print(draw)
+        show_hangman(tries_left)
+        print(word_completion)
+
+        # if guess_message not in 
+        # If letter is wrong: add 1 with tries and display the new stage
+
+while True:
+    game(choose_word())
